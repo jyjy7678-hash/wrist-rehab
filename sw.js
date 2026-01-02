31367e38ff02edb1c1a8584f5302d76dfe80559b
jyjy7678-hash <jyjy7678@gmail.com>
2026-01-02 15:39:38 +0900

    add rehab app files

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "wrist-rehab-v1";
+const ASSETS = ["./index.html","./manifest.webmanifest","./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      return resp;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
